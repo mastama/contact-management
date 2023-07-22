b07f59c870a25dfccdaa4e01b18ca23c96ce4ab5
Singgih <singgihpratama96@gmail.com>
2023-07-22 14:39:14 +0700

    membuat table dan entity User

--- database.sql
+++ database.sql
@@ -1,3 +1,16 @@
 CREATE DATABASE contact_management;
 
 use contact_management;
+
+create table users(
+                      username varchar(100) not null ,
+                      password varchar(100) not null ,
+                      name varchar(100) not null ,
+                      token varchar(100),
+                      token_expired_at BIGINT,
+                      primary key (username),
+                      UNIQUE (token)
+);
+
+select * from users;
+desc users;
---
+++ src/main/java/teatech/contactmanagement/entity/User.java
@@ -0,0 +1,31 @@
+package teatech.contactmanagement.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tbl_users")
+public class User {
+
+    @Id
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private String token;
+
+    @Column(name = "token_expired_at")
+    private Long tokenExpiredAt;
+}

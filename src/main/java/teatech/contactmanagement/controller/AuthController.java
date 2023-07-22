9ccc3b7b8efde165371bdfc3b06851e947c48fb5
Singgih <singgihpratama96@gmail.com>
2023-07-23 00:10:54 +0700

    mengubah response Auth login

--- src/main/java/teatech/contactmanagement/controller/AuthController.java
+++ src/main/java/teatech/contactmanagement/controller/AuthController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import teatech.contactmanagement.dto.RequestUserLogin;
+import teatech.contactmanagement.dto.ResponseService;
 import teatech.contactmanagement.dto.ResponseToken;
 import teatech.contactmanagement.dto.WebResponse;
 import teatech.contactmanagement.service.AuthService;
+import teatech.contactmanagement.util.Constant;
 
 @RestController
 @Slf4j
@@ -23,10 +25,19 @@ public class AuthController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public WebResponse<ResponseToken> login(@RequestBody RequestUserLogin req) {
+    public ResponseService login(@RequestBody RequestUserLogin req) {
         log.info("Start controller login: {}", req.getUsername());
         ResponseToken responseToken = authService.login(req);
+        String responseCode = Constant.RESPONSE.APPROVED.getCode();
+        String responseDesc = Constant.RESPONSE.APPROVED.getDescription();
+        Object data = responseToken;
+
+        ResponseService response = new ResponseService();
+        response.setResponseCode(responseCode);
+        response.setResponseDesc(responseDesc);
+        response.setData(data);
         log.info("End controller login: {}", req.getUsername());
-        return WebResponse.<ResponseToken>builder().data(responseToken).build();
+//        return WebResponse.<ResponseToken>builder().data(responseToken).build();
+        return response;
     }
 }
--- src/main/java/teatech/contactmanagement/dto/ResponseService.java
+++ src/main/java/teatech/contactmanagement/dto/ResponseService.java
@@ -1,8 +1,17 @@
 package teatech.contactmanagement.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseService {
 
     private String responseCode;
     private String responseDesc;
-    private String data;
+    private Object data;
 }
